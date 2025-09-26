faba1abdbcb563b1504a35b418915f8ba8383696
Loganathan <32327384+LoganathanThiyagaraj@users.noreply.github.com>
2025-09-26 11:03:20 +0530

    Delete payment_system/core/googlepay_processor.py

--- payment_system/core/googlepay_processor.py
+++
@@ -1,6 +0,0 @@
-# payment_system/processors/googlepay_processor.py
-# from payment_system.core.interfaces import PaymentStrategy # Uncomment if using ABC
-
-class GooglePayProcessor: # Inherit from PaymentStrategy if using ABC
-    def process_payment(self, amount: float):
-        print(f"Processing GooglePay payment of ${amount:.2f} via GooglePay API...")
