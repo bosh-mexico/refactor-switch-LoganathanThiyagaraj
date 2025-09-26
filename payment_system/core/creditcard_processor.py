dfbcd8ff1e1f6e0ec7f6abe77614a7a054aa9e63
Loganathan <32327384+LoganathanThiyagaraj@users.noreply.github.com>
2025-09-26 11:03:11 +0530

    Delete payment_system/core/creditcard_processor.py

--- payment_system/core/creditcard_processor.py
+++
@@ -1,6 +0,0 @@
-# payment_system/processors/creditcard_processor.py
-# from payment_system.core.interfaces import PaymentStrategy # Uncomment if using ABC
-
-class CreditCardProcessor: # Inherit from PaymentStrategy if using ABC
-    def process_payment(self, amount: float):
-        print(f"Processing Credit Card payment of ${amount:.2f} via Credit Card Gateway...")
