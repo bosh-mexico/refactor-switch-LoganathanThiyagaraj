2b24945520ad733ade7671897657277074f143ea
Loganathan <32327384+LoganathanThiyagaraj@users.noreply.github.com>
2025-09-26 11:03:29 +0530

    Delete payment_system/core/paypal_processor.py

--- payment_system/core/paypal_processor.py
+++
@@ -1,6 +0,0 @@
-# payment_system/processors/paypal_processor.py
-# from payment_system.core.interfaces import PaymentStrategy # Uncomment if using ABC
-
-class PayPalProcessor: # Inherit from PaymentStrategy if using ABC
-    def process_payment(self, amount: float):
-        print(f"Processing PayPal payment of ${amount:.2f} via PayPal API...")
